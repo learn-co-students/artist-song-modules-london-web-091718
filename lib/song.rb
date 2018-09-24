376ded789fb627ebfe76050788f847dbc47d103a
BarneyLoosemore <biscuitfull@gmail.com>
2018-09-24 09:25:04 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramble
   attr_accessor :name
@@ -9,9 +10,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
